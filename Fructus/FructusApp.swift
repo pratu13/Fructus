71a29afb23281fa51d336ba580ccf2c81b6b7943
pratu13 <pratu0906@gmail.com>
2021-05-04 15:23:17 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Pratyush  on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
